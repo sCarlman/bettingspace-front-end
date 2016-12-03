a40129a94066620f93682439db000752b1c93a35
sCarlman <carl.albertsson@gmail.com>
2016-12-03 13:20:00 +0000

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bettingSpaceApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the bettingSpaceApp
+ */
+angular.module('bettingSpaceApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bettingSpaceApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the bettingSpaceApp
+ */
+angular.module('bettingSpaceApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
