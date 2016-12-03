c7994be2d9ce60ecb88981369c371fed22b49cdb
sCarlman <carl.albertsson@gmail.com>
2016-12-03 16:11:25 +0000

    shit commit

---
+++ app/scripts/controllers/background.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bettingSpaceApp.controller:BackgroundCtrl
+ * @description
+ * # BackgroundCtrl
+ * Controller of the bettingSpaceApp
+ */
+angular.module('bettingSpaceApp')
+  .controller('BackgroundCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
